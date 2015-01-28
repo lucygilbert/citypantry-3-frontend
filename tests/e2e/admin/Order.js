761e3ef0a09b3ce3dfac3946558d89473d8bcb32
Amy Boyd <amy@amyboyd.co.uk>
2015-01-28 00:06:47 +0000

    Fix admin e2e tests.

--- tests/e2e/admin/Order.js
+++ tests/e2e/admin/Order.js
@@ -4,12 +4,19 @@ describe('Admin - order page', function() {
     beforeEach(function() {
         if (isFirst) {
             loginAsUser('alice@bunnies.test');
-            browser.get('/admin/order/1');
+            browser.get('/admin/orders');
+            element.all(by.css('#orders-table a[href^="/admin/order/"]')).first().click();
+            browser.driver.wait(function() {
+                return browser.driver.getCurrentUrl().then(function(url) {
+                    return (/\/admin\/order\/\d+$/.test(url));
+                });
+            });
             isFirst = false;
         }
     });
 
     it('should show the "order" page', function() {
-        expect(element(by.css('h1')).getText()).toBe('Order 1');
+        expect(browser.getCurrentUrl()).toMatch(/\/admin\/order\/\d+$/);
+        expect(element(by.css('h1')).getText()).toMatch(/^Order \d+$/);
     });
 });
--- tests/e2e/admin/Orders.js
+++ tests/e2e/admin/Orders.js
@@ -15,57 +15,57 @@ describe('Admin - orders page', function() {
     });
 
     it('should have 9 columns', function() {
-        gridTestUtils.expectHeaderColumnCount('order.gridOptions', 9);
+        gridTestUtils.expectHeaderColumnCount('orders-table', 9);
     });
 
     it('should have 2 rows', function() {
-        gridTestUtils.expectRowCount('order.gridOptions', 2);
+        gridTestUtils.expectRowCount('orders-table', 2);
     });
 
     it('should have the column name "Order No"', function() {
-        gridTestUtils.expectHeaderCellValueMatch('order.gridOptions', 0, 'Order No');
+        gridTestUtils.expectHeaderCellValueMatch('orders-table', 0, 'Order No');
     });
 
     it('should have the column name "Order Date"', function() {
-        gridTestUtils.expectHeaderCellValueMatch('order.gridOptions', 1, 'Order Date');
+        gridTestUtils.expectHeaderCellValueMatch('orders-table', 1, 'Order Date');
     });
 
     it('should have the column name "Delivery Date"', function() {
-        gridTestUtils.expectHeaderCellValueMatch('order.gridOptions', 2, 'Delivery Date');
+        gridTestUtils.expectHeaderCellValueMatch('orders-table', 2, 'Delivery Date');
     });
 
     it('should have the column name "Customer"', function() {
-        gridTestUtils.expectHeaderCellValueMatch('order.gridOptions', 3, 'Customer');
+        gridTestUtils.expectHeaderCellValueMatch('orders-table', 3, 'Customer');
     });
 
     it('should have the column name "Vendor"', function() {
-        gridTestUtils.expectHeaderCellValueMatch('order.gridOptions', 4, 'Vendor');
+        gridTestUtils.expectHeaderCellValueMatch('orders-table', 4, 'Vendor');
     });
 
     it('should have the column name "Package"', function() {
-        gridTestUtils.expectHeaderCellValueMatch('order.gridOptions', 5, 'Package');
+        gridTestUtils.expectHeaderCellValueMatch('orders-table', 5, 'Package');
     });
 
     it('should have the column name "Amount"', function() {
-        gridTestUtils.expectHeaderCellValueMatch('order.gridOptions', 6, 'Amount');
+        gridTestUtils.expectHeaderCellValueMatch('orders-table', 6, 'Amount');
     });
 
     it('should have the column name "Status"', function() {
-        gridTestUtils.expectHeaderCellValueMatch('order.gridOptions', 7, 'Status');
+        gridTestUtils.expectHeaderCellValueMatch('orders-table', 7, 'Status');
     });
 
     it('should have the column name "Action"', function() {
-        gridTestUtils.expectHeaderCellValueMatch('order.gridOptions', 8, 'Action');
+        gridTestUtils.expectHeaderCellValueMatch('orders-table', 8, 'Action');
     });
 
     it('should find 1 package when filtered by "Carrots"', function() {
-        gridTestUtils.enterFilterInColumn('order.gridOptions', 5, 'Car');
-        gridTestUtils.expectRowCount('order.gridOptions', 1);
-        gridTestUtils.expectCellValueMatch('order.gridOptions', 0, 5, 'Carrots');
+        gridTestUtils.enterFilterInColumn('orders-table', 5, 'Car');
+        gridTestUtils.expectRowCount('orders-table', 1);
+        gridTestUtils.expectCellValueMatch('orders-table', 0, 5, 'Carrots');
     });
 
     it('should find 2 packages when filter is cancelled', function() {
-        gridTestUtils.cancelFilterInColumn('order.gridOptions', 5);
-        gridTestUtils.expectRowCount('order.gridOptions', 2);
+        gridTestUtils.cancelFilterInColumn('orders-table', 5);
+        gridTestUtils.expectRowCount('orders-table', 2);
     });
 });
