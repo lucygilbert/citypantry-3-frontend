f6c72f44a23a76fa5470ee8b21a0469d5e0785f0
Thomas Ingram <thomasaingram@gmail.com>
2015-01-30 17:45:09 +0000

    Edit and delete package.

--- src/web/js/config.js
+++ src/web/js/config.js
@@ -4,7 +4,7 @@ angular.module('cp.controllers.admin').config(function($routeProvider) {
     $routeProvider.
         when('/admin/vendor/:vendorId', {
             controller: 'AdminEditVendorController',
-            templateUrl: '/dist/templates/vendor.html'
+            templateUrl: '/dist/templates/admin/edit-vendor.html'
         }).
         when('/admin/package/:packageId', {
             controller: 'AdminEditPackageController',
