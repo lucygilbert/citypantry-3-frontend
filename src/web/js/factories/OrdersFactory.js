0cc7600d142c92965ef8669abd46c2f3ca0e30ee
Amy Boyd <amy@amyboyd.co.uk>
2015-01-31 17:20:37 +0000

    Admin - order messages.

--- src/web/js/factories/OrdersFactory.js
+++ src/web/js/factories/OrdersFactory.js
@@ -6,6 +6,9 @@ angular.module('cp.factories').factory('OrdersFactory', function(API_BASE, ApiSe
         getOrder: function(id) {
             return ApiService.get(`${API_BASE}/orders/${id}`);
         },
+        getOrderMessages: function(id) {
+            return ApiService.get(`${API_BASE}/orders/${id}/messages`);
+        },
         deleteOrder: function(id) {
             return ApiService.delete(`${API_BASE}/order/${id}`);
         }
--- tests/e2e/admin/Order.js
+++ tests/e2e/admin/Order.js
@@ -1,10 +1,12 @@
 describe('Admin - order page', function() {
+    var gridTestUtils = require('../lib/gridTestUtils.spec.js');
     var isFirst = true;
 
     beforeEach(function() {
         if (isFirst) {
             loginAsUser('alice@bunnies.test');
             browser.get('/admin/orders');
+            gridTestUtils.enterFilterInColumn('orders-table', 5, 'Car');
             element.all(by.css('#orders-table a[href^="/admin/order/"]')).first().click();
             browser.driver.wait(function() {
                 return browser.driver.getCurrentUrl().then(function(url) {
@@ -18,5 +20,9 @@ describe('Admin - order page', function() {
     it('should show the "order" page', function() {
         expect(browser.getCurrentUrl()).toMatch(/\/admin\/order\/\d+$/);
         expect(element(by.css('h1')).getText()).toMatch(/^Order \d+$/);
+    });
+
+    it('should show the messages sent between the customer and the vendor', function() {
+        expect(element.all(by.repeater('message in messages')).count()).toBe(3);
     });
 });
