0d122ed7243b371c92658e8f52ddcf9a522e99d9
Thomas Ingram <thomasaingram@gmail.com>
2015-02-03 15:58:44 +0000

    Edit order.

--- src/web/js/controllers/admin/edit-order.controller.js
+++ src/web/js/controllers/admin/edit-order.controller.js
@@ -1,10 +1,89 @@
 angular.module('cp.controllers.admin').controller('AdminEditOrderController',
         function($scope, $routeParams, OrdersFactory, NotificationService) {
-    OrdersFactory.getOrder($routeParams.orderId)
-        .success(order => $scope.order = order)
-        .error(response => NotificationService.notifyError(response.data.errorTranslation));
-
-    OrdersFactory.getOrderMessages($routeParams.orderId)
-        .success(response => $scope.messages = response.messages)
-        .error(response => NotificationService.notifyError(response.data.errorTranslation));
+    $scope.headCountOptions = [];
+    $scope.headCount;
+    $scope.messages = [];
+    $scope.order = {};
+    $scope.save = save;
+    $scope.vegetarianHeadCountOptions = [];
+    $scope.vegetarianHeadCount;
+    
+    init();
+    
+    function init() {
+        OrdersFactory.getOrder($routeParams.orderId)
+            .success(function(order) {
+                $scope.order = order;
+                $scope.headCountOptions = getHeadCountOptions($scope.order.headCount, $scope.order.package.maxPeople, $scope.order.package.minPeople);
+                $scope.headCount = setHeadCountSelect($scope.order.headCount, $scope.headCountOptions);
+                $scope.vegetarianHeadCountOptions = getVegetarianHeadCountOptions($scope.order.headCount);
+                $scope.vegetarianHeadCount = setVegetarianHeadCountSelect($scope.order.vegetarianHeadCount, $scope.vegetarianHeadCountOptions);
+            })
+            .error(response => NotificationService.notifyError(response.data.errorTranslation));
+        
+        OrdersFactory.getOrderMessages($routeParams.orderId)
+            .success(response => $scope.messages = response.messages)
+            .error(response => NotificationService.notifyError(response.data.errorTranslation));
+        
+        $scope.$watch('headCount', function(updatedHeadCount) {
+            $scope.vegetarianHeadCountOptions = getVegetarianHeadCountOptions(updatedHeadCount);
+            $scope.vegetarianHeadCount = setVegetarianHeadCountSelect($scope.vegetarianHeadCount, $scope.vegetarianHeadCountOptions);
+        });
+    }
+    
+    function getHeadCountOptions(headCount, maxPeople, minPeople) {
+        var headCountOptions = [];
+        var headCount = headCount || 1;
+        var i;
+        var maxPeople = maxPeople || 1;
+        var minPeople = minPeople || 1;
+        
+        for (i = minPeople; i <= maxPeople; i += 1) {
+            headCountOptions.push(i);
+        }
+        
+        return headCountOptions;
+    }
+    
+    function getVegetarianHeadCountOptions(headCount) {
+        var headCount = headCount || 1;
+        var i;
+        var vegetarianHeadCountOptions = [];
+        
+        for (i = 1; i <= headCount; i += 1) {
+            vegetarianHeadCountOptions.push(i);
+        }
+        
+        return vegetarianHeadCountOptions;
+    }
+    
+    function setHeadCountSelect(headCount, options) {
+        var headCountIndex;
+        var headCount = headCount || 1;
+        var options = options || [];
+        
+        headCountIndex = options.indexOf(headCount);
+        return options[headCountIndex];
+    }
+    
+    function setVegetarianHeadCountSelect(vegetarianHeadCount, options) {
+        var options = options || [];
+        var vegetarianHeadCountIndex;
+        var vegetarianHeadCount = vegetarianHeadCount || 0;
+        
+        vegetarianHeadCountIndex = options.indexOf(vegetarianHeadCount);
+        return options[vegetarianHeadCountIndex];
+    }
+    
+    function save() {
+        var updatedOrder = {
+            date: $scope.order.date,
+            headCount: $scope.headCount,
+            pickupDate: $scope.order.pickupDate,
+            vegetarianHeadCount: $scope.vegetarianHeadCount
+        };
+        OrdersFactory.updateOrder($routeParams.orderId, updatedOrder)
+            .success(() => NotificationService.notifySuccess('The order has been edited.'))
+            .catch(response => NotificationService.notifyError(response.data.errorTranslation));
+    }
 });
--- tests/e2e/admin/Order.js
+++ tests/e2e/admin/Order.js
@@ -1,6 +1,7 @@
 describe('Admin - order page', function() {
     var gridTestUtils = require('../lib/gridTestUtils.spec.js');
     var isFirst = true;
+    var notificationModal = require('../NotificationModal.js');
 
     beforeEach(function() {
         if (isFirst) {
@@ -24,5 +25,34 @@ describe('Admin - order page', function() {
 
     it('should show the messages sent between the customer and the vendor', function() {
         expect(element.all(by.repeater('message in messages')).count()).toBe(3);
+    });
+    
+    it('should load the order details', function() {
+        // @todo – expected values.
+        expect(element(by.model('order.date')).getAttribute('value')).toBe('');
+        expect(element(by.model('headCount')).getAttribute('value')).toBe('');
+        expect(element(by.model('vegetarianHeadCount')).getAttribute('value')).toBe('');
+    });
+    
+    it('should convert the date and time to ISO8601', function() {
+        element(by.model('order.date')).clear().sendKeys('01/01/2016 12:30');
+        expect(element(by.model('order.date')).getAttribute('value')).toBe('2016-01-01T12:30:00+0000');
+    });
+    
+    it('should update the options for vegetarian head count when head count is changed', function() {
+        var options = element.all(by.css('#vegetarian_head_count > option'));
+        // @todo – expected count.
+        expect(options.count()).toBe();
+        element(by.model('headCount')).clear().sendKeys('11');
+        options = element.all(by.css('#vegetarian_head_count > option'));
+        expect(options.count()).toBe(11);
+    });
+    
+    it('should be able to save changes', function() {
+        element(by.model('headCount')).clear().sendKeys('11');
+        element(by.css('main form .btn.btn-primary')).click();
+        notificationModal.expectIsOpen();
+        notificationModal.expectSuccessHeader();
+        notificationModal.expectMessage('The order has been edited.');
     });
 });
