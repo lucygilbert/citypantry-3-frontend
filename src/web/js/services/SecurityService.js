bc0e4dbaae8ed94f6a1496e55e7dfa3ed6f40fb6
Thomas Ingram <thomasaingram@gmail.com>
2015-02-13 14:17:47 +0000

    Changes based on feedback.

--- src/web/js/services/SecurityService.js
+++ src/web/js/services/SecurityService.js
@@ -1,27 +1,49 @@
 angular.module('cp.services').service('SecurityService', function($location, $cookies) {
-    function isLoggedIn() {
-        return !!$cookies.userId;
-    }
-
-    function getUser() {
-        return JSON.parse(localStorage.getItem('user'));
-    }
-
     return {
+        getUser: function() {
+            return (localStorage.getItem('user')) ? JSON.parse(localStorage.getItem('user')) : false;
+        },
+
+        inGroup: function(groups) {
+            var userGroup = this.getUser()['group']['name'];
+            if (!userGroup) {
+                return false;
+            }
+
+            var result = false;
+
+            if (groups.constructor === Array) {
+                groups.forEach(group => {
+                    if (group.toLowerCase() === userGroup.toLowerCase()) {
+                        result = true;
+                        return false;
+                    }
+                });
+            } else {
+                result = (groups.toLowerCase() === userGroup.toLowerCase());
+            }
+
+            return result;
+        },
+
+        isLoggedIn: function() {
+            return !!$cookies.userId;
+        },
+
         requireLoggedIn: function() {
-            if (!isLoggedIn()) {
+            if (!this.isLoggedIn()) {
                 $location.path('/login');
             }
         },
 
         requireLoggedOut: function() {
-            if (isLoggedIn()) {
+            if (this.isLoggedIn()) {
                 $location.path('/');
             }
         },
 
         requireStaff: function() {
-            if (!isLoggedIn() || getUser()['group']['name'] !== 'staff') {
+            if (!this.isLoggedIn() || this.getUser()['group']['name'] !== 'staff') {
                 $location.path('/');
             }
         }
