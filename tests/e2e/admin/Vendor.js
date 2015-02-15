e79e48a508159e0f00f4599dd6a80aa66eb98811
Amy Boyd <amy@amyboyd.co.uk>
2015-02-15 18:02:44 +0000

    Show vendor addresses in 'edit vendor' page.

--- tests/e2e/admin/Vendor.js
+++ tests/e2e/admin/Vendor.js
@@ -1,10 +1,13 @@
-describe('Admin - vendor page', function() {
+var gridTestUtils = require('../lib/gridTestUtils.spec.js');
+
+describe('Admin - edit vendor page', function() {
     var isFirst = true;
 
     beforeEach(function() {
         if (isFirst) {
             loginAsUser('alice@bunnies.test');
             browser.get('/admin/vendors');
+            gridTestUtils.enterFilterInColumn('vendors-table', 1, 'Hong Tin');
             element.all(by.css('#vendors-table a[href^="/admin/vendor/"]')).first().click();
             browser.driver.wait(function() {
                 return browser.driver.getCurrentUrl().then(function(url) {
@@ -16,7 +19,11 @@ describe('Admin - vendor page', function() {
     });
 
     it('should show the "vendor" page', function() {
-        expect(browser.getCurrentUrl()).toMatch(/\/admin\/vendor\/[\da-f]+$/);
-        expect(element(by.css('h1')).getText()).toMatch(/^Vendor [\da-f]+$/);
+        expect(browser.getCurrentUrl()).toMatch(/\/admin\/vendor\/[\da-f]{24}$/);
+        expect(element(by.css('h1')).getText()).toMatch(/^Vendor \d+$/);
+    });
+
+    it('should show the vendor\'s addresses', function() {
+        expect(element.all(by.repeater('address in vendor.addresses')).count()).toBe(1);
     });
 });
