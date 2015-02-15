569f69927dc648bb5552da069750ad776f24c15c
Amy Boyd <amy@amyboyd.co.uk>
2015-02-15 17:30:09 +0000

    Fix addressSingleLineFormatter filter erroring on undefined.

--- src/web/js/filters/addressSingleLineFormatterFilter.js
+++ src/web/js/filters/addressSingleLineFormatterFilter.js
@@ -1,10 +1,16 @@
 angular.module('cp.filters', []);
 
 angular.module('cp.filters').filter('addressSingleLineFormatter', function() {
-    return address => address.addressLine1 +
-        (address.addressLine2 ? ', ' + address.addressLine2 : '') +
-        (address.addressLine3 ? ', ' + address.addressLine3 : '') +
-        (address.city ? ', ' + address.city : '') +
-        (address.postcode ? ', ' + address.postcode : '') +
-        (address.countryName ? ', ' + address.countryName : '');
+    return address => {
+        if (!address) {
+            return '';
+        }
+
+        return address.addressLine1 +
+            (address.addressLine2 ? ', ' + address.addressLine2 : '') +
+            (address.addressLine3 ? ', ' + address.addressLine3 : '') +
+            (address.city ? ', ' + address.city : '') +
+            (address.postcode ? ', ' + address.postcode : '') +
+            (address.countryName ? ', ' + address.countryName : '');
+    };
 });
