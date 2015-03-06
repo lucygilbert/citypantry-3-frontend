2145e7db08feb84804d814859b5b468c7bad12f3
Amy Boyd <amy@amyboyd.co.uk>
2015-03-06 13:55:02 +0000

    Add missing method VendorsFactory.deleteVendor().

--- src/web/js/factories/VendorsFactory.js
+++ src/web/js/factories/VendorsFactory.js
@@ -31,6 +31,8 @@ angular.module('cp.factories').factory('VendorsFactory', function(ApiService, AP
 
         updateVendor: (id, updatedVendor) => ApiService.put(`${API_BASE}/vendors/${id}`, updatedVendor),
 
-        updateSelf: (updatedVendor) => ApiService.put(`${API_BASE}/vendors/me`, updatedVendor)
+        updateSelf: (updatedVendor) => ApiService.put(`${API_BASE}/vendors/me`, updatedVendor),
+
+        deleteVendor: (id) => ApiService.delete(`${API_BASE}/vendors/${id}`)
     };
 });
