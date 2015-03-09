46ef956174ba358be1c7fc8d2865cd61e40ecc4f
Amy Boyd <amy@amyboyd.co.uk>
2015-03-09 11:48:13 +0000

    Customer orders page.

---
+++ tests/e2e/customer/Orders.js
@@ -0,0 +1,22 @@
+ddescribe('Customer orders', function() {
+    var first = true;
+
+    beforeEach(function() {
+        if (first) {
+            first = false;
+            loginAsUser('customer@bunnies.test');
+            browser.get('/customer/orders');
+        }
+    });
+
+    it('should show the orders page', function() {
+        expect(element(by.css('h1')).getText()).toBe('Your orders');
+    });
+
+    it('should have two orders', function() {
+        var orders = element.all(by.repeater('order in orders'));
+        expect(orders.count()).toBe(2);
+        expect(orders.get(0).getText()).toContain('Marshmallows');
+        expect(orders.get(1).getText()).toContain('Carrots');
+    });
+});
