3769ae4e08d71c137931be86ca0dea1bcac4bb60
Amy Boyd <amy@amyboyd.co.uk>
2015-03-24 13:25:45 +0000

    Edit packages page: let cuisine type be edited.

--- src/web/js/controllers/admin/AdminEditPackagesController.js
+++ src/web/js/controllers/admin/AdminEditPackagesController.js
@@ -1,19 +1,25 @@
 angular.module('cp.controllers.admin').controller('AdminEditPackagesController',
         function($scope, PackagesFactory, NotificationService, DocumentTitleService, SecurityService,
-        LoadingService) {
+        LoadingService, $q) {
     SecurityService.requireStaff();
+    DocumentTitleService('Edit packages');
 
     $scope.packagingTypes = PackagesFactory.getPackagingTypeOptions();
 
     $scope.noticeOptions = PackagesFactory.getNoticeOptions();
 
-    PackagesFactory.getAllPackages()
+    const loadingPromise1 = PackagesFactory.getCuisineTypes()
+        .success(response => $scope.cuisineTypes = response.cuisineTypes)
+        .error(response => NotificationService.notifyError(response.data.errorTranslation));
+
+    const loadingPromise2 = PackagesFactory.getAllPackages()
         .success(response => {
             $scope.packages = response.packages.sort((a, b) => a.name.localeCompare(b.name));
-            LoadingService.hide();
-            DocumentTitleService('Edit packages');
+            $scope.packages.forEach(pkg => pkg.cuisineTypeId = (pkg.cuisineType ? pkg.cuisineType.id : null));
         })
         .error(response => NotificationService.notifyError(response.data.errorTranslation));
+
+    $q.all([loadingPromise1, loadingPromise2]).then(() => LoadingService.hide());
 
     $scope.save = function(pkg) {
         if (!pkg.hasChanged) {
@@ -28,6 +34,7 @@ angular.module('cp.controllers.admin').controller('AdminEditPackagesController',
             packagingType: pkg.packagingType,
             canDeliverCutleryAndServiettes: pkg.canDeliverCutleryAndServiettes,
             notice: pkg.notice,
+            cuisineType: pkg.cuisineTypeId
         };
         PackagesFactory.updatePackage(pkg.id, updatedPackage)
             .success(response => {
