5dc87191fbf1461d0061a0c75b28154d3e26b1b2
Amy Boyd <amy@amyboyd.co.uk>
2015-03-26 10:30:14 +0000

    New header design.

--- tests/e2e/general/NavigationMenu.js
+++ tests/e2e/general/NavigationMenu.js
@@ -1,5 +1,3 @@
-// TODO: Assert result of choosing these navigation options
-// navMenuLinks.filter(elem => elem.getText().then(text => text === "MY ACCOUNT")).click();
 describe('Navigation Menu', function() {
     var navMenu, navMenuLinks, myAccountButton, myAccountMenu, myAccountMenuLinks;
 
@@ -14,77 +12,78 @@ describe('Navigation Menu', function() {
     it('should present customers with the appropriate navigation options', function() {
         loginAsUser('customer@bunnies.test');
 
-        expect(navMenu.getText()).toContain('MY ACCOUNT');
-        expect(navMenu.getText()).toContain('FAQ');
-        expect(navMenu.getText()).toContain('EVENT QUOTE');
-        expect(navMenu.getText()).toContain('Call us');
-        expect(navMenuLinks.count()).toBe(4);
+        expect(navMenu.getText()).toContain('Dashboard');
+        expect(navMenu.getText()).toContain('My Account');
+        expect(navMenu.getText()).toContain('020');
+        expect(navMenuLinks.count()).toBe(3);
 
         expect(myAccountMenu.isDisplayed()).toBe(false);
         myAccountButton.click();
         expect(myAccountMenu.isDisplayed()).toBe(true);
 
+        expect(myAccountMenu.getText()).toContain('Account Details');
         expect(myAccountMenu.getText()).toContain('Orders');
         expect(myAccountMenu.getText()).toContain('Addresses');
         expect(myAccountMenu.getText()).toContain('Cards');
-        expect(myAccountMenu.getText()).toContain('My details');
-        expect(myAccountMenu.getText()).toContain('Log out');
-        expect(myAccountMenuLinks.count()).toBe(5);
+        expect(myAccountMenu.getText()).toContain('My Details');
+        expect(myAccountMenu.getText()).toContain('Log Out');
+        expect(myAccountMenuLinks.count()).toBe(6);
     });
 
     it('should present vendors with the appropriate navigation options', function() {
         loginAsUser('vendor@bunnies.test');
 
-        expect(navMenu.getText()).toContain('MY ACCOUNT');
-        expect(navMenu.getText()).toContain('ORDERS');
-        expect(navMenu.getText()).toContain('ADDRESSES');
-        expect(navMenu.getText()).toContain('PROFILE');
-        expect(navMenu.getText()).toContain('PACKAGES');
-        expect(navMenu.getText()).toContain('CREATE PACKAGE');
-        expect(navMenuLinks.count()).toBe(6);
+        expect(navMenu.getText()).toContain('My Account');
+        expect(navMenu.getText()).toContain('Orders');
+        expect(navMenu.getText()).toContain('Addresses');
+        expect(navMenu.getText()).toContain('Profile');
+        expect(navMenu.getText()).toContain('Packages');
+        expect(navMenu.getText()).toContain('Create Package');
+        expect(navMenu.getText()).toContain('020');
+        expect(navMenuLinks.count()).toBe(7);
 
         expect(myAccountMenu.isDisplayed()).toBe(false);
         myAccountButton.click();
         expect(myAccountMenu.isDisplayed()).toBe(true);
 
-        expect(myAccountMenu.getText()).toContain('Delivery radiuses');
-        expect(myAccountMenu.getText()).toContain('Supplier agreement');
-        expect(myAccountMenu.getText()).toContain('My details');
-        expect(myAccountMenu.getText()).toContain('Log out');
+        expect(myAccountMenu.getText()).toContain('Delivery Radiuses');
+        expect(myAccountMenu.getText()).toContain('Supplier Agreement');
+        expect(myAccountMenu.getText()).toContain('My Details');
+        expect(myAccountMenu.getText()).toContain('Log Out');
         expect(myAccountMenuLinks.count()).toBe(4);
     });
 
     it('should present staff with the appropriate navigation options', function() {
         loginAsUser('alice@bunnies.test');
 
-        expect(navMenu.getText()).toContain('MY ACCOUNT');
-        expect(navMenu.getText()).toContain('ORDERS');
-        expect(navMenu.getText()).toContain('COURIER');
-        expect(navMenu.getText()).toContain('INVOICES');
-        expect(navMenu.getText()).toContain('PACKAGES');
-        expect(navMenu.getText()).toContain('VENDORS');
-        expect(navMenu.getText()).toContain('CUSTOMERS');
-        expect(navMenu.getText()).toContain('USERS');
+        expect(navMenu.getText()).toContain('My Account');
+        expect(navMenu.getText()).toContain('Orders');
+        expect(navMenu.getText()).toContain('Courier');
+        expect(navMenu.getText()).toContain('Invoices');
+        expect(navMenu.getText()).toContain('Packages');
+        expect(navMenu.getText()).toContain('Vendors');
+        expect(navMenu.getText()).toContain('Customers');
+        expect(navMenu.getText()).toContain('Users');
         expect(navMenuLinks.count()).toBe(8);
 
         expect(myAccountMenu.isDisplayed()).toBe(false);
         myAccountButton.click();
         expect(myAccountMenu.isDisplayed()).toBe(true);
 
-        expect(myAccountMenu.getText()).toContain('Edit packages');
-        expect(myAccountMenu.getText()).toContain('Edit vendors');
-        expect(myAccountMenu.getText()).toContain('My details');
-        expect(myAccountMenu.getText()).toContain('Log out');
+        expect(myAccountMenu.getText()).toContain('Edit Packages');
+        expect(myAccountMenu.getText()).toContain('Edit Vendors');
+        expect(myAccountMenu.getText()).toContain('My Details');
+        expect(myAccountMenu.getText()).toContain('Log Out');
         expect(myAccountMenuLinks.count()).toBe(4);
     });
 
     it('should present the default navigation options to people who aren\'t logged in', function() {
         logout();
 
-        expect(navMenu.getText()).toContain('LOG IN / SIGN UP');
+        expect(navMenu.getText()).toContain('Log In / Sign Up');
         expect(navMenu.getText()).toContain('FAQ');
-        expect(navMenu.getText()).toContain('EVENT QUOTE');
-        expect(navMenu.getText()).toContain('Call us');
+        expect(navMenu.getText()).toContain('Event Quote');
+        expect(navMenu.getText()).toContain('020');
         expect(navMenuLinks.count()).toBe(4);
 
         expect(myAccountMenu.isPresent()).toBe(false);
