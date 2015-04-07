7183b3f95efedaf4f2776b1dd092ceda488dbfe6
Amy Boyd <amy@amyboyd.co.uk>
2015-04-07 19:25:55 +0100

    Break large 'it' block into multiple smaller blocks.

--- tests/e2e/customer/Addresses.js
+++ tests/e2e/customer/Addresses.js
@@ -20,13 +20,15 @@ describe('Customer delivery addresses', function() {
         expect(addresses.get(0).getText()).toContain('25 Lena Gardens, London, W6 7PY, United Kingdom');
     });
 
-    it('should be able to add an address', function() {
+    it('should be able to open the "add address" page', function() {
         var addAddressButton = element.all(by.css('main p a')).get(0);
         expect(addAddressButton.getText()).toBe('ADD A NEW ADDRESS');
         addAddressButton.click();
 
         expect(browser.getCurrentUrl()).toMatch(/customer\/addresses\/new$/);
+    });
 
+    it('should be able to fill in the "add address" form', function() {
         element(by.model('address.addressLine1')).sendKeys('11 Francis Street');
         element(by.model('address.city')).sendKeys('London');
         element(by.model('address.postcode')).sendKeys('SW1P 1AA');
@@ -37,28 +39,34 @@ describe('Customer delivery addresses', function() {
         // The order notification mobile number, and contact name, is only for vendors.
         expect(element(by.model('address.orderNotificationMobileNumber')).isPresent()).toBe(false);
         expect(element(by.model('address.contactName')).isPresent()).toBe(false);
+    });
 
+    it('should be able to save the "add address" form', function() {
         element(by.css('main input.btn-primary')).click();
 
         expect(browser.getCurrentUrl()).toMatch(/customer\/addresses$/);
 
         notificationModal.expectIsOpen();
-        notificationModal.expectSuccessHeader();
         notificationModal.expectMessage('Address saved');
+        notificationModal.expectSuccessHeader();
         notificationModal.dismiss();
+    });
 
+    it('should list the new address in the address list', function() {
         var addresses = element.all(by.repeater('address in addresses'));
         expect(addresses.count()).toBe(2);
         expect(addresses.get(1).getText()).toContain('11 Francis Street, London, SW1P 1AA, United Kingdom');
     });
 
-    it('should be able to edit an address', function() {
+    it('should be able to open the "edit address" page', function() {
         var editAddressButton = element.all(by.css('.cp-edit-address')).get(0);
         expect(editAddressButton.getText()).toBe('EDIT');
         editAddressButton.click();
 
         expect(browser.getCurrentUrl()).toMatch(/customer\/addresses\/[0-9a-f]{24}$/);
+    });
 
+    it('should be able to fill in the "edit address" form', function() {
         expect(element(by.model('address.addressLine2')).getAttribute('value')).toBe('25 Lena Gardens');
         expect(element(by.model('address.city')).getAttribute('value')).toBe('London');
 
@@ -68,7 +76,9 @@ describe('Customer delivery addresses', function() {
 
         element(by.model('address.city')).clear().sendKeys('Albuquerque');
         element(by.model('address.companyName')).clear().sendKeys('Some Company');
+    });
 
+    it('should be able to save the "add address" form', function() {
         element(by.css('main input.btn-primary')).click();
 
         notificationModal.expectIsOpen();
@@ -77,7 +87,9 @@ describe('Customer delivery addresses', function() {
         notificationModal.dismiss();
 
         expect(browser.getCurrentUrl()).toMatch(/customer\/addresses$/);
+    });
 
+    it('should show the edited address details in the address list', function() {
         var addresses = element.all(by.repeater('address in addresses'));
         expect(addresses.get(0).getText()).toContain('25 Lena Gardens, Albuquerque, W6 7PY, United Kingdom');
     });
