25e9f0bc54a0f6d73a710d7cfb448ccc9081b60c
Lucy Gilbert-Boyd <lucygilbert01@gmail.com>
2015-04-26 16:21:53 +0100

    Add delete address button.

--- src/web/js/controllers/vendor/VendorPortalAddressesController.js
+++ src/web/js/controllers/vendor/VendorPortalAddressesController.js
@@ -1,9 +1,27 @@
 angular.module('cp.controllers.general').controller('VendorPortalAddressesController',
-        function ($scope, VendorsFactory, LoadingService, DocumentTitleService) {
+        function ($scope, AddressFactory, VendorsFactory, LoadingService, DocumentTitleService, NotificationService) {
     DocumentTitleService('Your addresses');
 
-    VendorsFactory.getAddresses().success(function(response) {
-        $scope.addresses = response.addresses;
-        LoadingService.hide();
-    });
+    function loadAddresses() {
+        VendorsFactory.getAddresses().success(response => {
+            $scope.addresses = response.addresses;
+            LoadingService.hide();
+        });
+    }
+
+    loadAddresses();
+
+    $scope.deleteAddress = address => {
+        LoadingService.show();
+
+        AddressFactory.deleteAddress(address.id)
+            .success(() => {
+                NotificationService.notifySuccess('Address deleted.');
+                loadAddresses();
+            })
+            .catch(response => {
+                NotificationService.notifyError(response.data.errorTranslation);
+                LoadingService.hide();
+            });
+    };
 });
