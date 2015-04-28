b42ba4fc9221fa9152074657585428c35329e190
Lucy Gilbert-Boyd <lucygilbert01@gmail.com>
2015-04-28 15:45:03 +0100

    Restyle vendors page.

--- tests/e2e/general/Vendors.js
+++ tests/e2e/general/Vendors.js
@@ -9,18 +9,14 @@ describe('Meet the vendors', function() {
         }
     });
 
-    it('should show the "Meet the vendors" page', function() {
-        expect(element(by.css('h1')).getText()).toBe('Meet the vendors');
-    });
-
     it('should show all the approved and active vendors, listed alphabetically', function() {
         var vendors = element.all(by.css('li.vendor'));
         expect(vendors.count()).toBe(5);
-        expect(vendors.get(0).element(by.css('h2')).getText()).toBe('Flame Mangal');
-        expect(vendors.get(1).element(by.css('h2')).getText()).toBe('Hong Tin');
-        expect(vendors.get(2).element(by.css('h2')).getText()).toBe('Kebab Centre');
-        expect(vendors.get(3).element(by.css('h2')).getText()).toBe('Oriental Kitchen Express');
-        expect(vendors.get(4).element(by.css('h2')).getText()).toBe('Sam\'s');
+        expect(vendors.get(0).element(by.css('h2')).getText()).toBe('FLAME MANGAL');
+        expect(vendors.get(1).element(by.css('h2')).getText()).toBe('HONG TIN');
+        expect(vendors.get(2).element(by.css('h2')).getText()).toBe('KEBAB CENTRE');
+        expect(vendors.get(3).element(by.css('h2')).getText()).toBe('ORIENTAL KITCHEN EXPRESS');
+        expect(vendors.get(4).element(by.css('h2')).getText()).toBe('SAM\'S');
     });
 
     it('should link to the individual vendor pages', function() {
