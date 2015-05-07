7afa9c091d321c4374b9bec6549adbb47acfc603
Amy Boyd <amy@amyboyd.co.uk>
2015-05-07 15:52:22 +0100

    Fix e2e admin tests that broke in the blitz of the last two weeks.

--- tests/e2e/admin/Invoice.js
+++ tests/e2e/admin/Invoice.js
@@ -25,7 +25,7 @@ describe('Admin - invoice page', function() {
         expect(invoice).toContain('Package: Paella');
         expect(invoice).toContain('25 Lena Gardens');
         expect(invoice).toMatch(/Food\/Drink £(\d*\.\d{2}|\d+) 15/); // Head count = 15.
-        expect(invoice).toContain('Subtotal £0.00');
+        expect(invoice).toContain('Subtotal £150.00');
         expect(invoice).toContain('VAT £0.00');
         expect(invoice).toContain('Grand Total £127.50');
     });
