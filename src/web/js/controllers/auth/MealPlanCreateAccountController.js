2add0bff01d7817e1be2dd70541412a6ea0d80ce
Thomas Ingram <thomasaingram@gmail.com>
2015-05-08 16:57:46 +0100

    Meal Plan create account.

---
+++ src/web/js/controllers/auth/MealPlanCreateAccountController.js
@@ -0,0 +1,49 @@
+angular.module('cp.controllers.authentication').controller('MealPlanCreateAccountController',
+        function($scope, $cookies, $routeParams, $window, SecurityService, DocumentTitleService,
+        LoadingService, NotificationService, UsersFactory) {
+    SecurityService.requireLoggedOut();
+    DocumentTitleService('Create account');
+
+    UsersFactory.getLoggedInUser($routeParams.userId, $routeParams.otat)
+        .success(response => {
+            $scope.user = response;
+            $cookies.userId = $scope.user.id;
+            $window.localStorage.setItem('user', JSON.stringify($scope.user));
+            LoadingService.hide();
+        })
+        .catch(response => NotificationService.notifyError(response.data.errorTranslation));
+
+    function clearPasswordFields() {
+        $scope.newPassword = '';
+        $scope.confirmPassword = '';
+    }
+
+    $scope.create = function() {
+        if ($scope.newPassword === $scope.confirmPassword) {
+            LoadingService.show();
+
+            $scope.createAccountError = null;
+
+            var passwords = {
+                newPassword: $scope.newPassword,
+                currentPassword: null
+            };
+
+            UsersFactory.changeOwnPassword(passwords)
+                .success(function(response) {
+                    $window.location = '/';
+                })
+                .catch(function(response) {
+                    $scope.createAccountError = response.data.errorTranslation;
+                    LoadingService.hide();
+                });
+        } else {
+            clearPasswordFields();
+            NotificationService.notifyError('The two passwords you have entered do not match.');
+        }
+    };
+
+    $scope.continue = function() {
+        $window.location = '/';
+    };
+});
---
+++ tests/unit/controllers/auth/MealPlanCreateAccountController.spec.js
@@ -0,0 +1,62 @@
+describe('MealPlanCreateAccountController', function() {
+    'use strict';
+
+    var scope;
+    var makeCtrl;
+    var cookies;
+    var LoadingService;
+    var NotificationService;
+    var UsersFactory;
+    var getLoggedInUserHttpRequest;
+
+    beforeEach(function() {
+        module('cp');
+    });
+
+    beforeEach(inject(function($controller, $rootScope, $cookies, _LoadingService_, _NotificationService_, _UsersFactory_) {
+        scope = $rootScope.$new();
+        cookies = $cookies;
+        LoadingService = _LoadingService_;
+        NotificationService = _NotificationService_;
+        UsersFactory = _UsersFactory_;
+
+        makeCtrl = function () {
+            $controller('MealPlanCreateAccountController', {
+                $scope: scope,
+            });
+        };
+    }));
+
+    describe('create', function() {
+        beforeEach(function() {
+            spyOn(LoadingService, 'show').and.returnValue();
+            spyOn(LoadingService, 'hide').and.returnValue();
+            spyOn(NotificationService, 'notifyError');
+            spyOn(UsersFactory, 'changeOwnPassword').and.returnValue(newPromise());
+
+            getLoggedInUserHttpRequest = newPromise({id: 'abc123'});
+            spyOn(UsersFactory, 'getLoggedInUser').and.returnValue(getLoggedInUserHttpRequest);
+        });
+
+        it('should set a cookie value to persist authentication', function() {
+            makeCtrl();
+            getLoggedInUserHttpRequest.resolveSuccess({id: 'abc123'});
+
+            expect(UsersFactory.getLoggedInUser).toHaveBeenCalled();
+            expect(scope.user).toEqual({id: 'abc123'});
+            expect(cookies.userId).toBe('abc123');
+            expect(NotificationService.notifyError).not.toHaveBeenCalled();
+        });
+
+        it('should call UsersFactory.changeOwnPassword with new password and current password', function() {
+            scope.newPassword = 'password';
+            scope.confirmPassword = 'password';
+
+            makeCtrl();
+            scope.create();
+
+            expect(UsersFactory.changeOwnPassword.calls.mostRecent().args)
+                .toEqual([{newPassword: 'password', currentPassword: null}]);
+        });
+    });
+});
