39f9658a155d43783b9849c3f269bcff2acdeee2
Thomas Ingram <thomasaingram@gmail.com>
2015-05-10 18:44:34 +0100

    Changes based on feedback.

--- src/web/js/controllers/auth/MealPlanCreateAccountController.js
+++ src/web/js/controllers/auth/MealPlanCreateAccountController.js
@@ -19,28 +19,29 @@ angular.module('cp.controllers.authentication').controller('MealPlanCreateAccoun
     }
 
     $scope.create = function() {
-        if ($scope.newPassword === $scope.confirmPassword) {
-            LoadingService.show();
-
-            $scope.createAccountError = null;
-
-            var passwords = {
-                newPassword: $scope.newPassword,
-                currentPassword: null
-            };
-
-            UsersFactory.changeOwnPassword(passwords)
-                .success(function(response) {
-                    $window.location = '/';
-                })
-                .catch(function(response) {
-                    $scope.createAccountError = response.data.errorTranslation;
-                    LoadingService.hide();
-                });
-        } else {
+        if ($scope.newPassword !== $scope.confirmPassword) {
             clearPasswordFields();
             NotificationService.notifyError('The two passwords you have entered do not match.');
+            return false;
         }
+
+        LoadingService.show();
+
+        $scope.createAccountError = null;
+
+        var passwords = {
+            newPassword: $scope.newPassword,
+            currentPassword: null
+        };
+
+        UsersFactory.changeOwnPassword(passwords)
+            .success(function(response) {
+                $window.location = '/';
+            })
+            .catch(function(response) {
+                $scope.createAccountError = response.data.errorTranslation;
+                LoadingService.hide();
+            });
     };
 
     $scope.continue = function() {
