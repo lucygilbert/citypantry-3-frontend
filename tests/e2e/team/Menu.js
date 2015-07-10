cb90e89c72f4141796f6f63465feedbb15e7228f
Amy Boyd <amy@amyboyd.co.uk>
2015-07-10 17:31:54 +0100

    Fix test broken by date formatter not working for 10 July.

--- tests/e2e/team/Menu.js
+++ tests/e2e/team/Menu.js
@@ -22,7 +22,7 @@ describe('Team menu', function() {
         var firstOrder = orders.get(0);
 
         var date = firstOrder.element(by.css('h2'));
-        expect(date.getText()).toMatch(/^\d [a-zA-Z]+ \d{4}, \d{2}:\d{2}$/);
+        expect(date.getText()).toMatch(/^\d+ [a-zA-Z]+ \d{4}, \d{2}:\d{2}$/);
 
         var packageDetail = firstOrder.element(by.css('cp-package-detail'));
         expect(packageDetail.isDisplayed()).toBe(true);
