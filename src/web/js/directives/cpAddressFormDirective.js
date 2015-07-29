55c484b6e6e70326a581839043d9261154ce9d84
Amy Boyd <amy@amyboyd.co.uk>
2015-07-29 11:28:45 +0100

    Cosmetics: reduce duplicate code in cpAddressForm directive.

--- src/web/js/directives/cpAddressFormDirective.js
+++ src/web/js/directives/cpAddressFormDirective.js
@@ -31,8 +31,17 @@ angular.module('cp').controller('cpAddressFormController', function($scope, $loc
         $scope.address.label = ($scope.address.label ? $scope.address.label : $scope.address.addressLine1);
     }
 
-    let isNew = !$scope.address.id;
-    $scope.isNew = isNew;
+    function redirectToAddressesPage() {
+        if (SecurityService.customerIsLoggedIn()) {
+            $location.path('/customer/addresses');
+        } else if (SecurityService.vendorIsLoggedIn()) {
+            $location.path('/vendor/addresses');
+        } else {
+            throw new Error('Unexpected user type.');
+        }
+    }
+
+    $scope.isNew = !$scope.address.id;
 
     setLabel();
 
@@ -51,27 +60,20 @@ angular.module('cp').controller('cpAddressFormController', function($scope, $loc
         setLabel();
 
         let promise;
-        if (isNew) {
-            promise = AddressFactory.createAddress($scope.address);
-            isNew = false;
-            $scope.isNew = false;
+        if ($scope.isNew) {
+            promise = AddressFactory.createAddress($scope.address)
+                .then(response => {
+                    $scope.isNew = false;
+                    return response;
+                });
         } else {
             promise = AddressFactory.updateAddress($scope.address.id, $scope.address);
         }
 
-        promise.then(function() {
-            const redirectTo = '/' + $scope.userType + '/addresses';
-            $location.path(redirectTo);
-        });
+        promise
+            .then(redirectToAddressesPage)
+            .catch(response => NotificationService.notifyError(response.data.errorTranslation));
     };
 
-    $scope.cancel = function() {
-        if (SecurityService.customerIsLoggedIn()) {
-            $location.path('/customer/addresses');
-        } else if (SecurityService.vendorIsLoggedIn()) {
-            $location.path('/vendor/addresses');
-        } else {
-            throw new Error('Unexpected user type.');
-        }
-    };
+    $scope.cancel = () => redirectToAddressesPage();
 });
