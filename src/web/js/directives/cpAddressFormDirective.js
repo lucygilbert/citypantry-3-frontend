0afaa5e97d7e09c9951acf04c589d6db536a1066
Amy Boyd <amy@amyboyd.co.uk>
2015-07-29 11:08:40 +0100

    Cosmetics: fix indentation in cpAddressForm directive.

--- src/web/js/directives/cpAddressFormDirective.js
+++ src/web/js/directives/cpAddressFormDirective.js
@@ -14,64 +14,64 @@ angular.module('cp').directive('cpAddressForm', function(SecurityService, getTem
     };
 });
 
-angular.module('cp').controller('cpAddressFormController',
-    function($scope, $location, AddressFactory, NotificationService, LoadingService, SecurityService) {
-            if ($scope.userType !== 'vendor' && $scope.userType !== 'customer') {
-                throw new Error('userType must be vendor or customer');
-            }
+angular.module('cp').controller('cpAddressFormController', function($scope, $location,
+        AddressFactory, NotificationService, LoadingService, SecurityService) {
+    if ($scope.userType !== 'vendor' && $scope.userType !== 'customer') {
+        throw new Error('userType must be vendor or customer');
+    }
 
-            if ($scope.userType === 'vendor') {
-                if (!$scope.address.orderNotificationMobileNumbers) {
-                    $scope.address.orderNotificationMobileNumbers = [];
-                }
-                $scope.address.orderNotificationMobileNumbersCommaSeparated = $scope.address.orderNotificationMobileNumbers.join(', ');
-            }
+    if ($scope.userType === 'vendor') {
+        if (!$scope.address.orderNotificationMobileNumbers) {
+            $scope.address.orderNotificationMobileNumbers = [];
+        }
+        $scope.address.orderNotificationMobileNumbersCommaSeparated = $scope.address.orderNotificationMobileNumbers.join(', ');
+    }
 
-            function setLabel() {
-                $scope.address.label = ($scope.address.label ? $scope.address.label : $scope.address.addressLine1);
-            }
+    function setLabel() {
+        $scope.address.label = ($scope.address.label ? $scope.address.label : $scope.address.addressLine1);
+    }
 
-            let isNew = !$scope.address.id;
-            $scope.isNew = isNew;
+    let isNew = !$scope.address.id;
+    $scope.isNew = isNew;
 
-            setLabel();
+    setLabel();
 
-            $scope.save = function() {
-                if ($scope.form.$invalid) {
-                    $scope.form.$submitted = true;
-                    return;
-                }
+    $scope.save = function() {
+        if ($scope.form.$invalid) {
+            $scope.form.$submitted = true;
+            return;
+        }
 
-                if ($scope.userType === 'vendor') {
-                    $scope.address.orderNotificationMobileNumbers = $scope.address.orderNotificationMobileNumbersCommaSeparated.split(/\s*,\s*/);
-                }
+        if ($scope.userType === 'vendor') {
+            $scope.address.orderNotificationMobileNumbers = $scope.address.orderNotificationMobileNumbersCommaSeparated.split(/\s*,\s*/);
+        }
 
-                LoadingService.show();
+        LoadingService.show();
 
-                setLabel();
+        setLabel();
 
-                let promise;
-                if (isNew) {
-                    promise = AddressFactory.createAddress($scope.address);
-                    isNew = false;
-                    $scope.isNew = false;
-                } else {
-                    promise = AddressFactory.updateAddress($scope.address.id, $scope.address);
-                }
+        let promise;
+        if (isNew) {
+            promise = AddressFactory.createAddress($scope.address);
+            isNew = false;
+            $scope.isNew = false;
+        } else {
+            promise = AddressFactory.updateAddress($scope.address.id, $scope.address);
+        }
 
-                promise.then(function() {
-                    const redirectTo = '/' + $scope.userType + '/addresses';
-                    $location.path(redirectTo);
-                });
-            };
+        promise.then(function() {
+            const redirectTo = '/' + $scope.userType + '/addresses';
+            $location.path(redirectTo);
+        });
+    };
 
-            $scope.cancel = function() {
-                if (SecurityService.customerIsLoggedIn()) {
-                    $location.path('/customer/addresses');
-                } else if (SecurityService.vendorIsLoggedIn()) {
-                    $location.path('/vendor/addresses');
-                } else {
-                    throw new Error('Unexpected user type.');
-                }
-            };
+    $scope.cancel = function() {
+        if (SecurityService.customerIsLoggedIn()) {
+            $location.path('/customer/addresses');
+        } else if (SecurityService.vendorIsLoggedIn()) {
+            $location.path('/vendor/addresses');
+        } else {
+            throw new Error('Unexpected user type.');
+        }
+    };
 });
